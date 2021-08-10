b986d3bac040f1e345998e7116537ee2706c29a4
Marco Grahl <manograhl@gmail.com>
2021-08-10 17:38:55 +0200

    added the map from mapbox and a helper wrapper

--- pages/city/index.js
+++ pages/city/index.js
@@ -3,13 +3,42 @@ import CityItem from '@/components/CityItem';
 import Layout from '@/components/Layout';
 const API_URL = process.env.NEXT_PUBLIC_API_URL
 const API_KEY = process.env.API_KEY
+import { useState } from 'react';
+import { useDebouncedValue } from '@/hooks/useDebouncedValue';
+import Map from '@/components/Map';
 
 export default function Cities({ city }) {
+    const [searchTerm, setSearchTerm] = useState(city.name)
+    // const debouncedText = useDebouncedValue(searchTerm, 300);
+    // hard coding styles just to see the Map
+    const style = {
+        minWidth: '600px',
+        height: '400px'
+    }
+
     // now the props get placed inside the component and we can render them on the page awesome
     console.log(city)
     return (
         <Layout>
+            <div>
+                <label htmlFor='search'>
+                    Find your City:
+                    <div>
+                        <input
+                            type="text"
+                            id="search"
+                            value={searchTerm}
+                            onChange={(e) => setSearchTerm(e.target.value)}
+                        />
+                    </div>
+
+                </label>
             <CityItem city={city} />
+            </div>
+            <section style={style}>
+                <Map {...city} />
+
+            </section>
         </Layout >
     );
 }
@@ -20,6 +49,7 @@ export default function Cities({ city }) {
 //  if you set revalidate: 1 it will, if it dosn't find anything, fetch again (1sec time it will take)
 // */
 export async function getStaticProps() {
+    // console.log(searchTerm)
     //make a request
     const search = 'Berlin'
     const res = await fetch(`${API_URL}/places/geoname?name=${search}&apikey=${API_KEY}`)
@@ -27,7 +57,10 @@ export async function getStaticProps() {
     // this is client side and gets shown inside the Terminal
 
 
-    console.log(city);
+    // get the Image thrught the id
+
+
+    // console.log(city);
     return {
         //  pass it from the server to the client side component
         props: { city },
