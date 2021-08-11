66f8cfa9bda3cfd5fee778a8556daf657584eae9
marcograhl <manograhl@gmail.com>
2021-08-11 21:58:19 +0200

    lifted up the state, and changig The state From the child component Through a funcion awesome (#6)

--- pages/historic/index.jsx
+++ pages/historic/index.jsx
@@ -1,14 +1,25 @@
 import IntItem from '@/components/IntItem';
 import Layout from '@/components/Layout';
 
+import { useState } from 'react';
+
 import Map from '@/components/Map';
 import { shuffle } from '@/library/helpers';
 import testData from '@/library/testData';
 
-
-
 export default function displayintPlaces({ intPlaces }) {
 // hard coding styles just to see the Map
+
+// with function I pass the needed props changes from the child to the parent
+const [selectedPlace, setSelectedPlace] = useState(null);
+
+let isList = true;
+//first view List, than changes if you click a pin
+if (selectedPlace) {
+  isList = false;
+} else {
+  isList = true;
+}
 
 
   const style = {
@@ -20,17 +31,20 @@ export default function displayintPlaces({ intPlaces }) {
   return (
     <Layout title="25 Berlin Intersting Places">
       <section style={style}>
-        <Map intPlaces={intPlaces}/>
-      </section>
-      
-      <div>
-{
-  intPlaces.length > 0 &&
-    intPlaces.map((intPlace) => (
-      <IntItem key={intPlace.xid} intPlace={intPlace} />
-    ))
-}
+<Map
+  intPlaces={intPlaces}
+  selectedPlace={selectedPlace}
+  changeSelected={(selectedPlace) => setSelectedPlace(selectedPlace)}
+/>
 
+      </section>
+
+      <div>
+        {isList && intPlaces.length > 0 &&
+          intPlaces.map((intPlace) => (
+            <IntItem key={intPlace.xid} intPlace={intPlace} />
+          ))}
+         {!isList && <IntItem intPlace={selectedPlace}/ > } 
       </div>
     </Layout>
   );
@@ -45,6 +59,7 @@ export async function getStaticProps() {
 // const API_URL = process.env.NEXT_PUBLIC_API_URL;
 // const API_KEY = process.env.API_KEY;
 
+
 //   /*
 //    * required Paramters
 //    */
@@ -52,6 +67,7 @@ export async function getStaticProps() {
 //   const lat = '52.52437';
 //   // we search in a radius is Large but the api will only display the 500 nearest To the radius Places
 // const searchRadius = '15000';
+
 
 //   /*
 //    * Optional Paramters
@@ -67,10 +83,10 @@ export async function getStaticProps() {
 //   const intPlaces = await res.json();
 
 
-
 // console.log(intPlaces.slice(0, 2));
 
-/*
+
+  /*
   it seems like he only gives back the results that are nearest to the radius center
   I wanted to get a random 25 places from Berlin and not the closest to the radius,
   I think I could have used other parameters for the search but I will make it work with
@@ -83,17 +99,17 @@ const filteredTestData = testData.filter(
   ({ name }) => !name.startsWith(searchTerm)
 );
 
+
 console.log(filteredTestData);
 const shuffledIntPlaces = shuffle(filteredTestData);
+
 
 // const shuffledIntPlaces = shuffle(intPlaces);
 
 
-
-
   return {
     //  pass it from the server to the client side component
-    props: { intPlaces: shuffledIntPlaces.slice(0,25) },
+    props: { intPlaces: shuffledIntPlaces.slice(0, 25) },
     revalidate: 1,
   };
 }
