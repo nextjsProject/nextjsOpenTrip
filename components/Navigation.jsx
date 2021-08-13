c73332fe98f2f10800b36541d479d9dc19a55db5
Marco Grahl <manograhl@gmail.com>
2021-08-13 08:08:12 +0200

    added a tip button and logic

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -7,8 +7,8 @@ export default function Navigation() {
         <a>Start</a>
       </Link>
 
-      <Link href="/historic">
-        <a>Historic</a>
+      <Link href="/Quiz">
+        <a>Quiz</a>
       </Link>
     </nav>
   );
