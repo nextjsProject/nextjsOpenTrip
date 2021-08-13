5eb573195b070fe29a0ac84afe86b384c9b181c8
Marco Grahl <manograhl@gmail.com>
2021-08-13 10:17:10 +0200

    li added type button

--- components/LiItem.jsx
+++ components/LiItem.jsx
@@ -17,7 +17,7 @@ export default function LiItem({question,intPlace}) {
 
     return (
         <>
-            <li className={isCorrect? 'correct':isCorrect===false ?'wrong': '' } onClick={() => setAnwser(question)}>{question.name}</li>
+            <li type='button'  className={isCorrect? 'correct':isCorrect===false ?'wrong': '' } onClick={() => setAnwser(question)}>{question.name}</li>
         </>
     )
 }
